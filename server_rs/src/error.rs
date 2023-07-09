9ab60bfff9f00c6b1defb72f99a4bec188fc0cba
Wiggidy <rlr0017@auburn.edu>
2023-07-09 15:17:34 -0500

    improved error readability

--- server_rs/src/error.rs
+++ server_rs/src/error.rs
@@ -31,7 +31,35 @@ impl From<FirestoreError> for Error {
 
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
+        match self {
+            Error::Firestore(e) => write!(f, "{:?}", e),
+            Error::SqliteDb(e) => write!(f, "{:?}", e),
+            Error::WeveEsi(e) => match e {
+                ProstTwirpError::AfterBodyError {
+                    body,
+                    method,
+                    version,
+                    headers,
+                    status,
+                    err,
+                } => {
+                    write!(
+                        f,
+                        "Body: {}, Method: {:?}, Version: {:?}, Headers: {:?}, Status: {:?}, Error: {:?}",
+                        match std::str::from_utf8(body) {
+                            Ok(s) => s.to_string(),
+                            Err(e) => e.to_string(),
+                        },
+                        method,
+                        version,
+                        headers,
+                        status,
+                        err,
+                    )
+                }
+                other => write!(f, "{:?}", other),
+            },
+        }
     }
 }
 
